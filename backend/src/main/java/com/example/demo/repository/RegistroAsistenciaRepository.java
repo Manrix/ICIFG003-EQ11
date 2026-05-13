c5155be2db65f59c12adb17b755211756416cca5
Manrix <bmanriquezn@gmail.com>
2026-05-12 23:56:24 -0400

    asistenciaRepository

--- backend/src/main/java/com/example/demo/repository/RegistroAsistenciaRepository.java
+++ backend/src/main/java/com/example/demo/repository/RegistroAsistenciaRepository.java
@@ -4,9 +4,12 @@ import com.example.demo.entity.RegistroAsistenciaEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RegistroAsistenciaRepository extends CrudRepository<RegistroAsistenciaEntity, Long> {
     List<RegistroAsistenciaEntity> findByAlumnoId(Long alumnoId);
+    Optional<RegistroAsistenciaEntity> findByAlumnoIdAndFecha(Long alumnoId, LocalDate fecha);
 }
